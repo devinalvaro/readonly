1d41a0cc4bd6281f3e3d14c1181621fe492e6d97
Devin Alvaro <devin.alvaro@gmail.com>
2024-04-30 06:48:43 +0800

    Check nested index

--- test/testdata/src/noflags/lib/struct.go
+++ test/testdata/src/noflags/lib/struct.go
@@ -1,11 +1,16 @@
 package lib
 
 type Struct struct {
-	Number        int `readonly:"enforce_all"`
+	Number int `readonly:"enforce_all"`
+
 	Pointer       *bool
 	NestedPointer **bool
-	Slice         []string
-	Map           map[string]struct{}
-	SlicePointer  *[]string
-	MapPointer    *map[string]struct{}
+
+	Slice        []string
+	SlicePointer *[]string
+	NestedSlice  [][]string
+
+	Map        map[string]struct{}
+	MapPointer *map[string]struct{}
+	NestedMap  map[string]map[string]struct{}
 }
--- test/testdata/src/noflags/main.go
+++ test/testdata/src/noflags/main.go
@@ -30,12 +30,20 @@ func main() {
 	strct.Slice = make([]string, 0) // want `readonly: field is being modified`
 	strct.Slice[0] = "any"          // want `readonly: field is being modified`
 
-	strct.Map = make(map[string]struct{}) // want `readonly: field is being modified`
-	strct.Map["any"] = struct{}{}         // want `readonly: field is being modified`
-
 	strct.SlicePointer = &strct.Slice // want `readonly: field is being modified`
 	(*strct.SlicePointer)[0] = "any"  // want `readonly: field is being modified`
 
+	strct.NestedSlice = make([][]string, 0)  // want `readonly: field is being modified`
+	strct.NestedSlice[0] = make([]string, 0) // want `readonly: field is being modified`
+	strct.NestedSlice[0][1] = "any"          // want `readonly: field is being modified`
+
+	strct.Map = make(map[string]struct{}) // want `readonly: field is being modified`
+	strct.Map["any"] = struct{}{}         // want `readonly: field is being modified`
+
 	strct.MapPointer = &strct.Map           // want `readonly: field is being modified`
 	(*strct.MapPointer)["any"] = struct{}{} // want `readonly: field is being modified`
+
+	strct.NestedMap = make(map[string]map[string]struct{}) // want `readonly: field is being modified`
+	strct.NestedMap["any"] = make(map[string]struct{})     // want `readonly: field is being modified`
+	strct.NestedMap["any"]["thing"] = struct{}{}           // want `readonly: field is being modified`
 }
